9c9f6321973c58c1296bb6b1998174a3787b1326
Beguide-code <claudebeguide2003@gmail.com>
2025-08-01 23:53:59 +0300

    play the game for a round
    created variables for score, created conditions
    for win and loss, and displayed them.

--- javascript.js
+++ javascript.js
@@ -1,20 +1,54 @@
+humanScore = 0;
+computerScore = 0;
+
 function getComputerChoice(){
     const randomNumber = Math.floor(Math.random()*3);
     if(randomNumber === 0){
-    console.log('rock');
+        return 'rock';
     }
     else if(randomNumber === 1){
-        console.log('paper');
+        return 'paper';
     }
     else{
-        console.log('scissors');
+        return 'scissors';
     }
 }
 
-getComputerChoice();
-
 const getHumanChoice = () => prompt('Enter rock, paper or scissors');
 
-const userChoice = getHumanChoice();
-console.log(userChoice);
+function playRound(humanChoice,computerChoice){
+    
+    if(humanSelection === 'rock' && computerSelection ==='paper'){
+        console.log('You lose! paper beats rock');
+        computerScore++;
+    }
+    else if(humanSelection === 'paper' && computerSelection ==='rock'){
+        console.log('You win! paper beats rock');
+        humanScore++;
+    }
+    else if(humanSelection === 'rock' && computerSelection ==='scissors'){
+        console.log('You win rock beats scissors');
+        humanScore++;
+    }
+    else if(humanSelection === 'scissors' && computerSelection ==='rock'){
+        console.log('You lose! rock beats scissors');
+        computerScore++;
+    }
+    else if(humanSelection === 'paper' && computerSelection ==='scissors'){
+        console.log('You lose! scissors beats paper');
+        computerScore++;
+    }
+    else if(humanSelection === 'scissors' && computerSelection ==='paper'){
+        console.log('You win! scissors beats paper');
+        humanScore++;
+    }
+    else
+        console.log('Equal guessing');
+
+}
+
+const humanSelection = getHumanChoice().toLowerCase();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection,computerSelection);
 
